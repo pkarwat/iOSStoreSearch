00486fcbdf654e8da39210db61594deb3f1a0924
Patryk <karwat.patryk@gmail.com>
2016-12-09 16:03:58 +0100

    test

--- StoreSearch/AppDelegate.swift
+++ StoreSearch/AppDelegate.swift
@@ -11,8 +11,9 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    //test
+    
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         customizeAppearance()
